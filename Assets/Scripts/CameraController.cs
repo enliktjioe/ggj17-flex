e0cad76fcad7210b9104281bc427b8b0a6acdc2a
Enlik Tjioe <enlik.stefanus@gmail.com>
2017-01-21 12:17:07 +1100

    Add camera controller to rotate the camera view, attached in Main Camera game object

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float turnSpeed = 4.0f;		// Speed of camera turning when mouse moves in along an axis
+
+	private Vector3 mouseOrigin;	// Position of cursor when mouse dragging starts
+	private bool isRotating;	// Is the camera being rotated?
+
+	void Update () 
+	{
+		// Get the left mouse button
+		if(Input.GetMouseButtonDown(0))
+		{
+			// Get mouse origin
+			mouseOrigin = Input.mousePosition;
+			isRotating = true;
+		}
+
+		// Disable movements on button release
+		if (!Input.GetMouseButton(0)) isRotating=false;
+
+		// Rotate camera along X and Y axis
+		if (isRotating)
+		{
+			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+			transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+			transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+		}
+
+	}
+}
